f389559cd0b5e16a5052b04c01a9a57f06a9bc7e
mohameedali06 <90595924+mohameedali06@users.noreply.github.com>
2021-12-10 13:02:00 +0530

    Delete E-Commerce-iPhone--main directory

--- E-Commerce-iPhone--main/src/main/java/com/ecommerce/main/repo/OrderDetailsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.main.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ecommerce.main.model.OrderDetails;
-
-public interface OrderDetailsRepo extends CrudRepository<OrderDetails, Integer> {
-	public OrderDetails findByOrderId(int orderId);
-}
--- E-Commerce-iPhone--main/src/main/java/com/ecommerce/main/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.main.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ecommerce.main.model.User;
-
-public interface UserRepo extends CrudRepository<User, Integer>{
-	public User findByUserEmail(String userEmail);
-}
